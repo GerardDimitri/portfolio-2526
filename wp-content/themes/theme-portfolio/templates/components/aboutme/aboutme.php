958fee114f5de51ae76c2dc16b22fe8be2a454fe
Gerard Dimitri <dimitri.gerard@student.hepl.be>
2026-04-17 17:18:41 +0200

    add: Add Svg

--- wp-content/themes/theme-portfolio/templates/components/aboutme/aboutme.php
+++ wp-content/themes/theme-portfolio/templates/components/aboutme/aboutme.php
@@ -7,36 +7,36 @@ $aboutme_desc = get_field('aboutme_desc');
 
 $path_title = get_field('path_title');
 ?>
-<section class="page__aboutme">
-    <div class="aboutme__container">
-        <div class="aboutme__content">
-            <?php if (!empty($aboutme_image)) : ?>
-                <div class="aboutme__image">
-                    <img src="<?= esc_url($aboutme_image['sizes']['large']); ?>"
-                         alt="<?= esc_attr($aboutme_image['alt']); ?>"
-                         width="<?= esc_attr($aboutme_image['sizes']['large-width']); ?>"
-                         height="<?= esc_attr($aboutme_image['sizes']['large-height']); ?>">
-                </div>
-            <?php endif; ?>
-            <?php if (!empty($aboutme_title)) : ?>
-                <h2 class="aboutme__title">
-                    <?= esc_html($aboutme_title) ?>
-                </h2>
-            <?php endif; ?>
+<main id="contenu" class="background-aboutme">
+    <section class="page__presentation">
+            <div class="aboutme__content">
+                <?php if (!empty($aboutme_image)) : ?>
+                    <div class="aboutme__image">
+                        <?= wp_get_attachment_image($aboutme_image['id'], 'medium', [
+                                'alt' => $aboutme_image['alt'],
+                                'lazyload' => true,
+                        ]) ?>
+                    </div>
+                <?php endif; ?>
+                <?php if (!empty($aboutme_title)) : ?>
+                    <h2 class="aboutme__title">
+                        <?= esc_html($aboutme_title) ?>
+                    </h2>
+                <?php endif; ?>
 
-            <?php if (!empty($aboutme_badge)) : ?>
-                <span class="aboutme__badge">
+                <?php if (!empty($aboutme_badge)) : ?>
+                    <span class="aboutme__badge">
                     <?= esc_html($aboutme_badge) ?>
                 </span>
+                <?php endif; ?>
+            </div>
+            <?php if (!empty($aboutme_desc)) : ?>
+                <p class="aboutme__desc">
+                    <?= esc_html($aboutme_desc) ?>
+                </p>
             <?php endif; ?>
-        </div>
-
-
-        <?php if (!empty($aboutme_desc)) : ?>
-            <p class="aboutme__desc">
-                <?= esc_html($aboutme_desc) ?>
-            </p>
-        <?php endif; ?>
-    </div>
-</section>
+    </section>
+    <?php get_template_part('templates/components/parcours/parcours'); ?>
+    <?php get_template_part('templates/components/skills/skills'); ?>
+</main>
 
