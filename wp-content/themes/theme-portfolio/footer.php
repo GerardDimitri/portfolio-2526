8a4ed39ae5c29afacec0519c2b7509a3000b3a45
Gerard Dimitri <dimitri.gerard@student.hepl.be>
2026-04-24 11:29:04 +0200

    refactor

--- wp-content/themes/theme-portfolio/footer.php
+++ wp-content/themes/theme-portfolio/footer.php
@@ -6,53 +6,50 @@ $utils_2 = dw_get_navigation_links('utils-2'); // ex: Cairn, Gallica
 ?>
 
 <footer class="footer" role="contentinfo">
-    <div class="footer__container">
-
-        <nav class="footer__menu" aria-label="Ressources techniques">
-            <h2 class="hidden">Menu de navigation de bas de page</h2>
-            <ul class="footer__list" role="list">
-                <li class="footer__item">
-                    <a class="footer__link" href="" target="_blank"
-                       rel="noopener">Ressources utiles</a>
-                    <ul>
-                        <li itemprop="sameAs">
-                            <a href="">test</a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="footer__item">
-                    <a class="footer__link" href="" target="_blank"
-                       rel="noopener">Navigation</a>
-                    <ul>
-                        <li itemprop="sameAs">
-                            <a href="">test</a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="footer__item">
-                    <a class="footer__link" href="" target="_blank"
-                       rel="noopener">Ressources utiles (encore)</a>
-                    <ul>
-                        <li itemprop="sameAs">
-                            <a href="">test</a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="footer__item">
-                    <a class="footer__link" href="" target="_blank"
-                       rel="noopener">Suivez-moi</a>
-                    <ul>
-                        <li itemprop="sameAs">
-                            <a href="">test</a>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-    </div>
-    <div class="footer__bottom">
-        <p class="footer__copyright">
-            © <?= date('Y'); ?> <strong>Dimitri Gerard</strong>. Tous droits réservés. Créé par Dimitri Gerard
-        </p>
-        <a href="<?= get_permalink(get_page_by_path('mention-legale')); ?>" class="footer__legal">Mentions légales</a>
-    </div>
+    <nav class="footer__menu" aria-label="Ressources techniques">
+        <h2 class="hidden">Menu de navigation de bas de page</h2>
+        <ul class="footer__list" role="list">
+            <li class="footer__item">
+                Ressources utiles
+                <ul>
+                    <li itemprop="sameAs">
+                        <a href="">test</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="footer__item">
+                Navigation
+                <ul>
+                    <li itemprop="sameAs">
+                        <a href="">test</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="footer__item">
+                Ressources utiles (encore)
+                <ul>
+                    <li itemprop="sameAs">
+                        <a href="">test</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="footer__item">
+                Suivez-moi
+                <ul>
+                    <li itemprop="sameAs">
+                        <a href="">test</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+        <div class="footer__bottom">
+            <div>
+                <p class="footer__copyright">
+                    © <?= date('Y'); ?> <strong>Dimitri Gerard</strong>. Tous droits réservés. Créé par Dimitri Gerard
+                </p>
+                <a href="<?= get_permalink(get_page_by_path('mention-legale')); ?>" class="footer__legal">Mentions
+                    légales</a>
+            </div>
+        </div>
+    </nav>
 </footer>
